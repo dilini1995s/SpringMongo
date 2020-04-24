f5416330bacf48cc8f1092ac4ee79250c2129b4e
dilini1995s <dilinitharanga721@gmail.com>
2020-04-24 09:34:34 +0530

    Initial commit

---
+++ src/main/java/com/example/Mongo/TutorialRepository.java
@@ -0,0 +1,11 @@
+package com.example.Mongo;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface TutorialRepository extends MongoRepository<Tutorial, String> {
+    List<Tutorial> findByTitleContaining(String title);
+    List<Tutorial> findByPublished(boolean published);
+}
